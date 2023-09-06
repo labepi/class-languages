1aba28372674dd2f6366fd8895e60225d19796b1
Joao Medeiros <jpsm1985@gmail.com>
2023-09-06 10:36:36 -0300

    Adds Expression automata

--- state-machine/zipcode.c
+++ state-machine/zipcode.c
@@ -50,6 +50,36 @@ void destroy_afd(struct afd *a)
     free(a->transitions);
 }
 
+void create_expr_afd(struct afd *a)
+{
+    unsigned int s;
+    char c;
+    a->n = 3;
+    a->start = 0;
+    a->final = 1;
+    a->transitions = malloc(a->n * sizeof(struct node *));
+
+    /*
+     * initializing empty lists
+     */
+    for (s = 0; s < a->n; s++)
+        a->transitions[s] = NULL;
+
+    for (c = '0'; c <= '9'; c++)
+        node_insert(&a->transitions[0], create_node(c, 1));
+
+    for (c = '0'; c <= '9'; c++)
+        node_insert(&a->transitions[1], create_node(c, 1));
+
+    node_insert(&a->transitions[1], create_node('-', 2));
+    node_insert(&a->transitions[1], create_node('+', 2));
+    node_insert(&a->transitions[1], create_node('/', 2));
+    node_insert(&a->transitions[1], create_node('*', 2));
+
+    for (c = '0'; c <= '9'; c++)
+        node_insert(&a->transitions[2], create_node(c, 1));
+}
+
 void create_zipcode_afd(struct afd *a)
 {
     unsigned int s;
@@ -67,13 +97,15 @@ void create_zipcode_afd(struct afd *a)
             node_insert(&a->transitions[s], create_node(i, s + 1));
 
     node_insert(&a->transitions[5], create_node('-', 6));
+    for (i = '0'; i <= '9'; i++)
+        node_insert(&a->transitions[5], create_node(i, 7));
 
     for (s = 6; s <= 8; s++)
         for (i = '0'; i <= '9'; i++)
             node_insert(&a->transitions[s], create_node(i, s + 1));
 }
 
-char match_zipcode(char *string, struct afd *a)
+char match(char *string, struct afd *a)
 {
     unsigned int i = 0;
     unsigned int s = a->start;
@@ -110,8 +142,8 @@ int main(int argc, char** argv)
 {
     struct afd a;
 
-    create_zipcode_afd(&a);
-    printf("%c\n", match_zipcode(argv[1], &a));
+    create_expr_afd(&a);
+    printf("%c\n", match(argv[1], &a));
     destroy_afd(&a);
 
     return 0;
